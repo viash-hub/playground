9e5f6391f8453a71e6ddcd5adb040b6e158f4e89
Nirmayi <ynirmayi@gmail.com>
2024-11-27 19:07:59 +0100

    update output

--- src/meta_workflows/demultiplex_rnaseq/test.sh
+++ src/meta_workflows/demultiplex_rnaseq/test.sh
@@ -4,6 +4,7 @@ viash ns build --setup cb --parallel -q demultiplex_rnaseq
 
 nextflow run . \
   -main-script target/nextflow/demultiplex_rnaseq/main.nf \
-  -params-file example.yaml \
+  -params-file src/meta_workflows/demultiplex_rnaseq/example.yaml \
   -profile docker \
   --publish_dir test_results \
+  --resume 
